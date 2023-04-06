30aa1b3a5e0619e07a80c9d15e901036802d315f
Poonam Chopade <chopadepoonam1199@gmail.com>
2023-04-06 16:23:04 +0530

    updated

--- src/components/FruitsData.jsx
+++ src/components/FruitsData.jsx
@@ -68,7 +68,7 @@ const FruitsData=[
         title:"Dragon Fruit",
         imglink:"https://media.istockphoto.com/id/503603245/photo/white-dragon-fruit.jpg?s=612x612&w=0&k=20&c=xwnU0d4gDNMEkMURWi-Eef9dWjOPYbV_8-kLOu1ONt4=",
         altname:"dragon fruit",
-        desc:"Pitaya usually refers to fruit of the genus Stenocereus, while pitahaya or dragon fruit refers to fruit of the genus Selenicereus (formerly Hylocereus), both in the family Cactaceae.[3] The common name in English – dragon fruit – derives from the leather-like skin and scaly spikes on the fruit exterior. Depending on the variety, pitaya fruits may have sweet- or sour-tasting flesh that can be red, white, or yellow in color.Dragon fruit is cultivated in Peru, Mexico, South Asia, Southeast Asia, East Asia, the United States, the Caribbean, Australia, Mesoamerica and throughout tropical and subtropical regions of the world.",
+        desc:"Pitaya usually refers to fruit of the genus Stenocereus, while pitahaya or dragon fruit refers to fruit of the genus Selenicereus (formerly Hylocereus), both in the family Cactaceae. The common name in English – dragon fruit – derives from the leather-like skin and scaly spikes on the fruit exterior. Depending on the variety, pitaya fruits may have sweet- or sour-tasting flesh that can be red, white, or yellow in color.Dragon fruit is cultivated in Peru, Mexico, South Asia, Southeast Asia, East Asia, the United States, the Caribbean, Australia, Mesoamerica and throughout tropical and subtropical regions of the world.",
         btn:"Read More"
     },
     {
@@ -76,7 +76,7 @@ const FruitsData=[
         title:"Pomegranate",
         imglink:"https://media.istockphoto.com/id/1271188960/photo/ripe-pomegranate-on-the-tree-in-autumn.jpg?s=612x612&w=0&k=20&c=_J04W279yZC44C1PdqDanJpL30hObkZ1Xgd51nI6gNs=",
         altname:"pomegranate",
-        desc:"The pomegranate (Punica granatum) is a fruit-bearing deciduous shrub in the family Lythraceae, subfamily Punicoideae, that grows between 5 and 10 m (16 and 33 ft) tall. The pomegranate was originally described throughout the Mediterranean region. It was introduced into Spanish America in the late 16th century and into California by Spanish settlers in 1769. The fruit is typically in season in the Southern Hemisphere from March to May, and in the Northern Hemisphere from September to February.[5][6] As intact sarcotestas or juice, pomegranates are used in baking, cooking, juice blends, meal garnishes, smoothies, and alcoholic beverages, such as cocktails and wine.",
+        desc:"The pomegranate (Punica granatum) is a fruit-bearing deciduous shrub in the family Lythraceae, subfamily Punicoideae, that grows between 5 and 10 m (16 and 33 ft) tall. The pomegranate was originally described throughout the Mediterranean region. It was introduced into Spanish America in the late 16th century and into California by Spanish settlers in 1769. The fruit is typically in season in the Southern Hemisphere from March to May, and in the Northern Hemisphere from September to February. As intact sarcotestas or juice, pomegranates are used in baking, cooking, juice blends, meal garnishes, smoothies, and alcoholic beverages, such as cocktails and wine.",
         btn:"Read More"
     },
 
